738edc85f076f6de84540316d42ebcd2ec8b68ab
marezloum <marezloum@gmail.com>
2022-12-01 00:24:55 +0300

    Add project files.

---
+++ 11 final 1/11 final 1.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A; char tmp[33];
+	cout << "Enter your number ";
+	cin >> A;
+	_itoa_s(A, tmp, 2);
+	int length = strlen(tmp);
+	if (tmp[length - 1] == '0' && tmp[length - 2] == '0' && tmp[length - 3] == '0' && tmp[length - 4] == '0') {
+		cout << "Number is a multiple of 16";
+	}
+	else {
+		cout << "Number is NOT a multiple of 16";
+	}
+
+}
+
